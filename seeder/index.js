b0ba09e1b7e0659d168a249d0329d05505d5ea71
bagussadewa12 <komangbagussadewanetyra@gmail.com>
2024-10-20 17:08:57 +0800

    seeder

--- seeder/index.js
+++ seeder/index.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose');
+const fs = require('fs');
+require('dotenv').config();
 
 async function main() {
   /**--------------- Not allowed to be edited - start - --------------------- */
@@ -18,16 +19,60 @@ async function main() {
   });
 
   // Define a schema for the collection
-  const schema = new mongoose.Schema({}, { strict: false });
+  const schema = new mongoose.Schema(
+    {
+      title: String,
+      year: Number,
+      genre: [String],
+      description: String,
+      director: String,
+      cast: [String],
+    },
+    { strict: false }
+  );
   const Model = mongoose.model(collection, schema);
 
   switch (command) {
-    case "check-db-connection":
+    case 'check-db-connection':
       await checkConnection();
       break;
+    case 'bulk-insert':
+      const data = JSON.parse(fs.readFileSync('seed.json', 'utf-8'));
+
+      for (const movie of data) {
+        const movieModel = new Model();
+        movieModel.title = movie.title;
+        movieModel.year = movie.year;
+        movieModel.genre = movie.genre;
+        movieModel.description = movie.description;
+        movieModel.director = movie.director;
+        movieModel.cast = movie.cast;
+
+        await movieModel.save();
+      }
+      console.log(data);
+
+      console.log('Bulk Insert Berhasil');
+
+      break;
+
+    case 'get-all':
+      const movieDataGetAll = await Model.find();
+
+      console.log(movieDataGetAll);
+      console.log('Get Data Berhasil');
+
+      break;
+
+    case 'reset-db':
+      await Model.deleteMany();
+      console.log('Reset Data Berhasil');
+
+      break;
+
     // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
     default:
-      throw Error("command not found");
+      throw Error('command not found');
   }
 
   await mongoose.disconnect();
@@ -35,14 +80,26 @@ async function main() {
 }
 
 async function checkConnection() {
-  console.log("check db connection started...");
+  console.log('check db connection started...');
   try {
     await mongoose.connection.db.admin().ping();
-    console.log("MongoDB connection is successful!");
+    console.log('MongoDB connection is successful!');
   } catch (err) {
-    console.error("MongoDB connection failed:", err);
+    console.error('MongoDB connection failed:', err);
   }
-  console.log("check db connection ended...");
+  console.log('check db connection ended...');
+}
+
+async function bulkInsert() {
+  const data = JSON.parse(fs.readFileSync('seed.json', 'utf-8'));
+
+  for (const movie of data) {
+    const movieModel = new Model();
+    movieModel.name = movie.name;
+  }
+  console.log(data);
+
+  console.log('Bulk Insert Berhasil');
 }
 
 main();
